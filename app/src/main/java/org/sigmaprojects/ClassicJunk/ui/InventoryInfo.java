3ff8a43ce7f54ae367408b8296185eccf999be45
Don Quist <don@sigmaprojects.org>
2016-02-22 23:36:33 -0800

    Theme change

--- app/src/main/java/org/sigmaprojects/ClassicJunk/ui/InventoryInfo.java
+++ app/src/main/java/org/sigmaprojects/ClassicJunk/ui/InventoryInfo.java
@@ -82,7 +82,7 @@ public class InventoryInfo extends Activity {
 
                 Picasso.with(this)
                         .load(inventory.getImageurl() + "?h=400")
-                                //.placeholder(R.drawable.car_image_placeholder)
+                         .placeholder(R.drawable.car_image_placeholder)
                         .error(R.drawable.car_image_error)
                         .into(carImage);
             } catch (Exception e) {
